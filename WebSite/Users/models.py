41c8ca7812a7b90870ac93540d4430d71431a05c
sina.kachoei <sina95kachoei@gmail.com>
2019-01-02 11:29:08 +0330

    test

--- WebSite/Users/models.py
+++ WebSite/Users/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
-asdasdasd
+
+class CustomUser(User):
+
+    def __str__(self):
+        return self.username
+
+class Profile(models.Model):
+
+	bio = models.TextField(max_length = 255, blank = True, null = True)	
+	user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, default="")
